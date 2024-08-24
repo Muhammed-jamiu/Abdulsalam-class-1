31ee69b77e072071e505604ad58822697cdff60e
Anate Muhammed <anatemuhammed@gmail.com>
2024-08-24 20:34:18 +0100

    first commit

---
+++ Abdulsalam.js
@@ -0,0 +1,17 @@
+// let box = document.getElementById("box");
+// let form = document.getElementById("form");
+// let formdetails = document.getElementById("form-details");
+
+// box.addEventListener(
+//   "mouseover",
+//   () => {
+//     form.style.backgroundColor = "black";
+//     form.style.border = "red";
+//     formdetails.style.color = "white";
+//   },
+//   box.addEventListener("mouseleave", () => {
+//     form.style.backgroundColor = "none";
+//     form.style.border = "none";
+//     formdetails.style.color = "none";
+//   })
+// );
